ea59ae54332b1b4b56631e81f8ade6bb826281df
behnoudng <behnoudng@gmail.com>
2023-10-07 13:05:52 +0330

    finished the version 1.0

---
+++ content.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const myDiv = document.querySelector('div[role="main"].my-div-class');
+
+    if (myDiv) {
+      myDiv.style.width = "960px";
+    }
+  });
